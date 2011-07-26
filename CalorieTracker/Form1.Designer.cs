720e2ef69ad9b6b401a0679e2b2a68ce84167c00
Patrick Speiser <doskir@gmail.com>
2011-07-26 14:43:13 +0200

    Changed Design a bit

--- CalorieTracker/Form1.Designer.cs
+++ CalorieTracker/Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("2011.07.25");
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("2011.07.25-31", new System.Windows.Forms.TreeNode[] {
-            treeNode5});
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("2011.07.25");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("2011.07.25-31", new System.Windows.Forms.TreeNode[] {
+            treeNode1});
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -41,13 +41,13 @@
             // 
             this.treeView1.Location = new System.Drawing.Point(13, 13);
             this.treeView1.Name = "treeView1";
-            treeNode5.Name = "Node5";
-            treeNode5.Text = "2011.07.25";
-            treeNode6.Name = "Node4";
-            treeNode6.Text = "2011.07.25-31";
+            treeNode1.Name = "Node5";
+            treeNode1.Text = "2011.07.25";
+            treeNode2.Name = "Node4";
+            treeNode2.Text = "2011.07.25-31";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode6});
-            this.treeView1.Size = new System.Drawing.Size(199, 350);
+            treeNode2});
+            this.treeView1.Size = new System.Drawing.Size(165, 350);
             this.treeView1.TabIndex = 0;
             // 
             // listView1
@@ -55,9 +55,9 @@
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2});
-            this.listView1.Location = new System.Drawing.Point(265, 13);
+            this.listView1.Location = new System.Drawing.Point(184, 12);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(191, 226);
+            this.listView1.Size = new System.Drawing.Size(236, 351);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -66,6 +66,7 @@
             // columnHeader1
             // 
             this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 171;
             // 
             // columnHeader2
             // 
@@ -75,7 +76,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(602, 459);
+            this.ClientSize = new System.Drawing.Size(430, 437);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.treeView1);
             this.Name = "Form1";
