f06e878a4ba2df706b9eca353791df3035864aa4
Patrick Speiser <doskir@gmail.com>
2011-07-26 13:49:59 +0200

    Added food loading.

--- CalorieTracker/Form1.Designer.cs
+++ CalorieTracker/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("2011.07.25");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("2011.07.25-31", new System.Windows.Forms.TreeNode[] {
+            treeNode3});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 13);
+            this.treeView1.Name = "treeView1";
+            treeNode3.Name = "Node5";
+            treeNode3.Text = "2011.07.25";
+            treeNode4.Name = "Node4";
+            treeNode4.Text = "2011.07.25-31";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            this.treeView1.Size = new System.Drawing.Size(199, 350);
+            this.treeView1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(265, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(191, 226);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Kcal";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 459);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
     }
 }
 
--- CalorieTracker/Form1.cs
+++ CalorieTracker/Form1.cs
@@ -6,14 +6,57 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace CalorieTracker
 {
     public partial class Form1 : Form
     {
+        private Sql sql;
+        private List<Food> foods = new List<Food>(); 
         public Form1()
         {
             InitializeComponent();
+            sql = new Sql();
+            LoadFoods();
+            UpdateFoodList();
         }
+        private void UpdateFoodList()
+        {
+            listView1.Items.Clear();
+            foreach(Food food in foods)
+            {
+                if (food.Hidden)
+                    continue;
+                ListViewItem lvi = new ListViewItem(food.Name);
+                lvi.SubItems.Add(food.TotalKcal.ToString());
+                listView1.Items.Add(lvi);
+            }
+        }
+        private void LoadFoods()
+        {
+            foods.Clear();
+            sql.SqlConnection.Open();
+            System.Data.SQLite.SQLiteCommand command = sql.SqlConnection.CreateCommand();
+            command.CommandText = "SELECT * FROM food ORDER BY name";
+            using(SQLiteDataReader reader = command.ExecuteReader())
+            {
+                if(reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string name = reader.GetString(1);
+                        int kcalPer100g = reader.GetInt32(2);
+                        int grams = reader.GetInt32(3);
+                        bool hidden = reader.GetBoolean(4);
+                        Food food = new Food(id, name, kcalPer100g, grams, hidden);
+                        foods.Add(food);
+                    }
+                }
+            }
+            sql.SqlConnection.Close();
+        }
+       
     }
 }
